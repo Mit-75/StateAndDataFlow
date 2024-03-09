11fb6903d7f8d8fb600edb492cdbacfcb01613f2
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-09 15:03:44 +0500

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Dmitry Parhomenko on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
